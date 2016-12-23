08d4be11f1a0f7cbde00ad1242874e833a8e34a5
Vlad-Online <vlad-online@mail.ru>
2016-12-23 17:17:30 +0300

    GroupList model

---
+++ src/mephilog/GroupListModel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mephilog;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vlad
+ */
+public class GroupListModel {
+    String title;
+    ArrayList<String> students;
+    
+    public void GroupListModel(String title) {
+        
+    }
+}
